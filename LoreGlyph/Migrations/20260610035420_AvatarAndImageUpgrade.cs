982f273aee907cc821e261e2f89235782bf6cd18
Springlezz <maria.bursyanina@yandex.ru>
2026-07-07 19:44:09 +0700

    migrations

---
+++ LoreGlyph/Migrations/20260610035420_AvatarAndImageUpgrade.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoreGlyph.Migrations
+{
+    /// <inheritdoc />
+    public partial class AvatarAndImageUpgrade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "image_path",
+                table: "Languages",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "image_path",
+                table: "Languages");
+        }
+    }
+}
