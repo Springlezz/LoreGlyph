7ae7184fc525009960f3f8ac8d1e3b7a4fd2636b
Springlezz <maria.bursyanina@yandex.ru>
2026-07-18 21:41:43 +0700

    Ability to add avatars, Docker modification for the ability to change avatars, production

--- LoreGlyph/Frontend/src/utils/url.js
+++ LoreGlyph/Frontend/src/utils/url.js
@@ -1,8 +1,11 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const fileUrl = (path) => {
-  if (!path) {
-    return null;
-  }
-  return `${API_URL}${path}`;
+    if (!path) {
+        return null;
+    }
+
+    const baseUrl = API_URL.replace(/\/api\/?$/, '');
+
+    return `${baseUrl}${path}`;
 };
